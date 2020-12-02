b83c83de7af234c0f11216caa0a126140f18d074
Pedro Fadel <pfadel@stone.com.br>
2020-12-02 15:27:10 -0300

    :rocket: implements second day problems in python as well

---
+++ 2020/2/main.py
@@ -0,0 +1,49 @@
+import os
+
+def firstproblem(payload):
+    valid = 0
+    lines = {x for x in payload.split('\n') if x}
+
+    for line in lines:
+        info = line.split(' ')
+        numbers = info[0].split('-')
+        min = int(numbers[0])
+        max = int(numbers[1])
+
+        letter = info[1][:len(':')]
+        count = 0
+        for c in info[2]:
+            if c == letter:
+                count+=1
+
+        if count<=max and count>=min:
+            valid+=1
+
+    return valid
+
+def secondproblem(payload):
+    valid = 0
+    lines = {x for x in payload.split('\n') if x}
+
+    for line in lines:
+        info = line.split(' ')
+        numbers = info[0].split('-')
+        first = int(numbers[0])
+        second = int(numbers[1])
+
+        letter = info[1][:len(':')]
+
+        if (info[2][first-1] == letter) != (info[2][second-1] == letter):
+            valid+=1
+
+    return valid
+
+
+if __name__ == "__main__":
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    with open(os.path.join(dir_path, 'input'), 'r') as entry:
+        payload = entry.read()
+
+    print(firstproblem(payload))
+    print(secondproblem(payload))
